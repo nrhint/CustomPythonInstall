1c2fb339a6dd7d5143e0af9f6eb746e0ecbb04a9
nrhint <nathan@hintonclan.org>
2018-11-28 14:38:20 -0700

    testing a custom import

--- Nathan.py
+++ Nathan.py
@@ -1,2 +1,7 @@
 print("Custom Nathan files imported!")
+print("Created by Nathan Hinton")
+print("Used by programs created by Nathan to do stuff.")
+print()
+print()
 
+#Execute code here.  Mostly imports for other things so that they can be accessed.

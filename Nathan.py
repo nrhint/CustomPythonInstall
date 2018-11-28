222e5a88315b2be84cff18728bbddd853fe11ed6
nrhint <nathan@hintonclan.org>
2018-11-28 14:15:41 -0700

    making work

---
+++ Nathan.py
@@ -0,0 +1,2 @@
+print("Custom Nathan files imported!")
+
---
+++ NathanInit.py
@@ -0,0 +1,8 @@
+#Nathan Hinton
+#Used for setting up custom imports
+
+
+##########  INSTRUCTIONS  #########################
+#Copy me to the folder a level up
+
+from ImportFilesPython import Nathan
